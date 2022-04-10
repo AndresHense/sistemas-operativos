c360240ef59ed1c1ee927c0940f5e1e85884da7c
AndresHense <andreshense@gmail.com>
2022-04-10 17:54:40 -0300

    take_1 client_alu.c

--- taller-2/sockets/client_alu.c
+++ taller-2/sockets/client_alu.c
@@ -7,61 +7,75 @@
 
 #include "config.h"
 
-int main(int argc, char **argv){
-	char bufrecv[MSGLEN + 1], *bufsend, *p;
-	struct sockaddr_in name;
-	size_t bufsendsiz=0;
-	ssize_t r, w;
-	int s;
-	bufsend = NULL;
-	memset(&name, 0, sizeof(name));
+int main(int argc, char **argv) {
+  char bufrecv[MSGLEN + 1], *bufsend, *p;
+  struct sockaddr_in name;
+  size_t bufsendsiz = 0;
+  ssize_t r, w;
+  int s;
+  bufsend = NULL;
+  memset(&name, 0, sizeof(name));
 
-	if (argc != 2) {
-		fprintf(stderr, "uso: %s ip\n", argv[0]);
-		exit(1);
-	}
+  if (argc != 2) {
+    fprintf(stderr, "uso: %s ip\n", argv[0]);
+    exit(1);
+  }
 
-	//Creamos el socket
-	if ((s = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-		perror("socket");
-		exit(1);
-	}
+  // Creamos el socket
+  if ((s = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+    perror("socket");
+    exit(1);
+  }
 
-	//Configuracion, no tocar
-	if (!inet_aton(argv[1], &name.sin_addr)) {
-		fprintf(stderr, "%s: ip desconocida\n", argv[1]);
-		exit(1);
-	}
+  // Configuracion, no tocar
+  if (!inet_aton(argv[1], &name.sin_addr)) {
+    fprintf(stderr, "%s: ip desconocida\n", argv[1]);
+    exit(1);
+  }
 
-	name.sin_family = AF_INET;
-	name.sin_port = htons(PORT);
+  name.sin_family = AF_INET;
+  name.sin_port = htons(PORT);
 
-	//COMPLETAR: conectar el socket
+  // COMPLETAR: conectar el socket
+  if (connect(s, (struct sockaddr *)&name, sizeof(name)) == -1) {
+    perror("conectandose");
+    exit(1);
+  }
 
+  // COMPLETAR: Recibir mensaje de bienvenida y ponerlo en bufrecv
+  r = recv(s, bufrecv, sizeof(bufrecv) - 1, 0);
+  if (r == -1) {
+    perror("recv");
+  } else {
+    bufrecv[r] = '\0';
+  }
+  printf("Bienvenida: %s\n", bufrecv);
 
-	//COMPLETAR: Recibir mensaje de bienvenida y ponerlo en bufrecv
+  for (;;) {
+    printf("[%s]> ", argv[1]);
+    // Tomamos el input
+    if ((w = getline(&bufsend, &bufsendsiz, stdin)) == -1) {
+      if (!feof(stdin))
+        perror("getline");
+      break;
+    }
+    // Nos fijamos si era ENDMSG para cortar la ejecucion
+    if (strncmp(bufsend, ENDMSG, w) == 0) {
+      break;
+    }
+    // COMPLETAR: Enviar el mensaje
+    if (send(s, bufrecv, strlen(bufrecv), 0) == -1) {
+      perror("enviando");
+      exit(1);
+    }
+    // COMPLETAR: ecibir mensajes hasta que envie  CMDSE
+    if (strncmp(bufsend, CMDSEP, w) != 0) {
+      printf("%s", bufsend);
+    }
+  }
 
-	printf("Bienvenida: %s\n",bufrecv);
+  free(bufsend);
+  close(s);
 
-	for (;;) {
-		printf("[%s]> ", argv[1]);
-		//Tomamos el input
-		if ((w = getline(&bufsend, &bufsendsiz, stdin)) == -1) {
-			if (!feof(stdin))
-				perror("getline");
-			break;
-		}
-		//Nos fijamos si era ENDMSG para cortar la ejecucion
-		if (strncmp(bufsend, ENDMSG, w) == 0){
-			break;
-		}
-		//COMPLETAR: Enviar el mensaje
-
-		//COMPLETAR: ecibir mensajes hasta que envie  CMDSE
-	}
-
-	free(bufsend);
-	close(s);
-
-	return 0;
+  return 0;
 }
